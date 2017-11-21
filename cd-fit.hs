c70a1d981ccd090c22d81c056e4983e3f0fce044
Qvintvs <emperorview@gmail.com>
2017-11-21 20:31:10 +0800

    update

--- cd-fit.hs
+++ cd-fit.hs
@@ -1,5 +1,8 @@
 import Text.ParserCombinators.Parsec
 import Data.List (sortBy)
+import Test.QuickCheck
+import Control.Monad (liftM2, replicateM)
+
 
 parseInput =
   do dirs <- many dirAndSize
@@ -7,7 +10,7 @@ parseInput =
      return dirs
 
 data Dir = Dir { dir_size::Int, dir_name::String } deriving Show
-data DirPack = DirPack { pack_size::Int, dirs::[Dir]} deriving Show
+data DirPack = DirPack { pack_size::Int, dirs::[Dir] } deriving Show
 
 dirAndSize =
   do size <- many1 digit
@@ -22,17 +25,19 @@ main = do input <- getContents
                           Left err -> error $ "Input:\n" ++ show input ++
                                               "\nError:\n" ++ show err
                           Right result -> result
-          putStrLn "debug: parsed:"; print dirs
+          putStrLn "debug: parsed:"; print (greedy_pack dirs)
 
 
-media_size = 700*1024*1024      -- 700MB CD volume
+-- media_size = 700*1024*1024      -- 700MB CD volume
+media_size = 25      -- 700MB CD volume
 
 greedy_pack dirs = foldl maybe_add_dir (DirPack 0 []) $ sortBy cmpSize dirs
-  where
-  cmpSize d1 d2 = compare (dir_size d1) (dir_size d2)
+  where cmpSize d1 d2 = compare (dir_size d1) (dir_size d2)
 
 -- helper function to add dir d to pack p
 maybe_add_dir p d =
   let new_size = pack_size p + dir_size d
       new_dirs = d:(dirs p)
       in if new_size > media_size then p else DirPack new_size new_dirs
+
+
